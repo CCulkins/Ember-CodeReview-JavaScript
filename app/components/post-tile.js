343e991c0941cd072b7c11fa058b0fd3b4bc92e7
Cassandra Culkins <cassy.culkins@gmail.com>
2016-04-02 20:51:46 -0700

    edit post-tile to allow user to see/hide full post

--- app/components/post-tile.js
+++ app/components/post-tile.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  fullPostDisplaying: false,
+  actions: {
+    fullPostDisplays: function() {
+      this.set('fullPostDisplaying', true);
+    },
+    fullPostNoDisplay: function() {
+      this.set('fullPostDisplaying', false);
+    }
+  }
 });
